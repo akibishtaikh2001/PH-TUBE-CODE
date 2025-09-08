28db646bc5445b097f93ef6476791f8013cceab3
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-08 18:44:18 +0600

    Render Conditional Data On Dynamic Card

---
+++ scripts/test.js
@@ -0,0 +1,11 @@
+const isverified = "";
+
+// if(isverified === true){
+//     console.log('user is verified');
+// }
+// else{
+//      console.log('user is not verified');
+// }
+console.log(`${isverified === true ? 'user is verified' : 'user is not verified'}`);
+ 
+  
